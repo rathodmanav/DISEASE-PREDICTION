e6ff47e8e04608c50101de35e52d48040ebe73b9
rathodmanav <rathodmanavv@gmail.com>
2025-06-23 12:24:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-diabetes_model = pickle.load(open('C:/Users/Manav/Desktop/Disease Prediction/diabetes_model.sav', "rb"))
-heart_disease_model = pickle.load(open('C:/Users/Manav/Desktop/Disease Prediction\heart_disease_model.sav', "rb"))
-parkinsons_model = pickle.load(open('C:/Users/Manav/Desktop/Disease Prediction\parkinsons_data.sav', "rb"))
+diabetes_model = pickle.load(open('diabetes_model.sav', "rb"))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', "rb"))
+parkinsons_model = pickle.load(open('parkinsons_data.sav', "rb"))
 
 # Sidebar menu
 with st.sidebar:
